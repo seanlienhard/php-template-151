48963224c5edc364b63c412b893bd70e01ec1f6d
Gibz User <sean.lienhard@outlook.com>
2017-06-29 16:19:03 +0200

    changed schema

--- schema.sql
+++ schema.sql
@@ -1,3 +1,6 @@
+DROP DATABASE IF EXISTS youtube;
+CREATE DATABASE youtube;
+USE youtube;
 -- Adminer 4.2.5 MySQL dump
 
 SET NAMES utf8;
@@ -5,59 +8,41 @@ SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
-DROP TABLE IF EXISTS `comment`;
-CREATE TABLE `comment` (
-  `commentId` int(11) NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `category`;
+CREATE TABLE `category` (
+  `categoryId` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(40) NOT NULL,
   `userId` int(11) NOT NULL,
-  `postId` int(11) NOT NULL,
-  `date` datetime NOT NULL,
-  `message` longtext NOT NULL,
-  PRIMARY KEY (`commentId`),
+  PRIMARY KEY (`categoryId`),
   KEY `userId` (`userId`),
-  KEY `postId` (`postId`),
-  CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `user` (`userId`),
-  CONSTRAINT `comment_ibfk_2` FOREIGN KEY (`postId`) REFERENCES `post` (`postId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-DROP TABLE IF EXISTS `like`;
-CREATE TABLE `like` (
-  `likeId` int(11) NOT NULL AUTO_INCREMENT,
-  `userId` int(11) NOT NULL,
-  `postId` int(11) NOT NULL,
-  PRIMARY KEY (`likeId`),
-  KEY `userId` (`userId`),
-  KEY `postId` (`postId`),
-  CONSTRAINT `like_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `user` (`userId`),
-  CONSTRAINT `like_ibfk_2` FOREIGN KEY (`postId`) REFERENCES `post` (`postId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-DROP TABLE IF EXISTS `post`;
-CREATE TABLE `post` (
-  `postId` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(45) NOT NULL,
-  `picture` longblob,
-  `type` varchar(45) DEFAULT NULL,
-  `text` longtext NOT NULL,
-  `date` datetime NOT NULL,
-  `userId` int(11) NOT NULL,
-  PRIMARY KEY (`postId`),
-  KEY `userId` (`userId`),
-  CONSTRAINT `post_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `user` (`userId`)
+  CONSTRAINT `category_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `user` (`userId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `userId` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(50) NOT NULL,
-  `mail` varchar(50) NOT NULL,
-  `password` varchar(110) NOT NULL,
-  `activated` tinyint(4) NOT NULL,
-  `confirmation` varchar(50) NOT NULL,
+  `username` varchar(45) DEFAULT NULL,
+  `mail` varchar(45) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `activated` tinyint(4) NOT NULL DEFAULT '0',
+  `confirmation` tinytext NOT NULL,
   PRIMARY KEY (`userId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
--- 2017-06-19 19:21:53
+DROP TABLE IF EXISTS `video`;
+CREATE TABLE `video` (
+  `videoId` int(11) NOT NULL AUTO_INCREMENT,
+  `link` varchar(100) NOT NULL,
+  `categoryId` int(11) NOT NULL,
+  `userId` int(11) NOT NULL,
+  PRIMARY KEY (`videoId`),
+  KEY `categoryId` (`categoryId`),
+  KEY `userId` (`userId`),
+  CONSTRAINT `video_ibfk_1` FOREIGN KEY (`categoryId`) REFERENCES `category` (`categoryId`),
+  CONSTRAINT `video_ibfk_2` FOREIGN KEY (`userId`) REFERENCES `user` (`userId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- 2017-06-22 13:05:18
